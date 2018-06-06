0e44e7e395964830a82b50fc87bad91751eb3836
戚羿辰 <416350144@qq.com>
2018-06-06 11:40:16 +0800

    change unlock rule

--- mvc-ethereum/src/main/java/com/mvc/ethereum/service/LockRecordService.java
+++ mvc-ethereum/src/main/java/com/mvc/ethereum/service/LockRecordService.java
@@ -49,6 +49,10 @@ public class LockRecordService extends BaseBiz<LockRecordMapper, LockRecord> {
 
     public void updateUnlock(Integer times) {
         lockRecordMapper.updateUnlock();
+        // 第一次解锁一半
+        if(times.equals(6)){
+            times = 2;
+        }
         lockRecordMapper.updateUnlockBalance(times);
     }
 }
